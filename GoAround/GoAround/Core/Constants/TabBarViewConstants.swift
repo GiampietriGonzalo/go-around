d1d121f18e5fa29858e71bbf13418db597b5aec1
Gonzalo Giampietri <giampietri.gonzalo@gmail.com>
2023-11-03 13:57:51 -0300

    Add CometUI package dependency
    - Remove PrimaryItemTabBarView files
    - Add CometUI as a dependency

--- GoAround/GoAround/Core/Constants/TabBarViewConstants.swift
+++ GoAround/GoAround/Core/Constants/TabBarViewConstants.swift
@@ -5,8 +5,9 @@
 //  Created by Gonzalo.Giampietri on 26/10/2023.
 //
 
+import CometUI
+
 struct TabBarViewConstants {
-    static let items: [TabBarItemType] = [.home, .trip, .add, .message, .profile]
     static let tabBarModel = PrimaryItemTabBarModel(items: [tabBarHomeItemWrapper,
                                                             tabBarTripItemWrapper,
                                                             tabBarAddItemWrapper,
@@ -14,17 +15,35 @@ struct TabBarViewConstants {
                                                             tabBarProfileItemWrapper])
     
     static let tabBarHomeItemWrapper = PrimayItemTabBarItemWrapperModel(item: TabBarViewConstants.tabBarHomeItemModel)
-    static let tabBarHomeItemModel = PrimaryItemTabBarSecondaryItemModel(tabPosition: 0, activeIconName: "homeIconActive", inactiveIconName: "homeIconInactive")
+    static let tabBarHomeItemModel = PrimaryItemTabBarSecondaryItemModel(tabPosition: 0,
+                                                                         activeIconName: "homeIconActive",
+                                                                         inactiveIconName: "homeIconInactive",
+                                                                         activeColor: .blueFull,
+                                                                         inactiveColor: .black)
     
     static let tabBarTripItemWrapper = PrimayItemTabBarItemWrapperModel(item: TabBarViewConstants.tabBarTripItemModel)
-    static let tabBarTripItemModel = PrimaryItemTabBarSecondaryItemModel(tabPosition: 1, activeIconName: "tripIconActive", inactiveIconName: "tripIconInactive")
+    static let tabBarTripItemModel = PrimaryItemTabBarSecondaryItemModel(tabPosition: 1,
+                                                                         activeIconName: "tripIconActive",
+                                                                         inactiveIconName: "tripIconInactive",
+                                                                         activeColor: .blueFull,
+                                                                         inactiveColor: .black)
     
     static let tabBarAddItemWrapper = PrimayItemTabBarItemWrapperModel(item: TabBarViewConstants.tabBarAddItemModel)
-    static let tabBarAddItemModel = PrimaryItemTabBarPrimaryItemModel(tabPosition: 2, gradientFirstColor: .blueFull, gradientLastColor: .blueLight300)
+    static let tabBarAddItemModel = PrimaryItemTabBarPrimaryItemModel(tabPosition: 2,
+                                                                      gradientFirstColor: .blueFull,
+                                                                      gradientLastColor: .blueLight300)
     
     static let tabBarMessageItemWrapper = PrimayItemTabBarItemWrapperModel(item: TabBarViewConstants.tabBarMessageItemModel)
-    static let tabBarMessageItemModel = PrimaryItemTabBarSecondaryItemModel(tabPosition: 3, activeIconName: "messageIconActive", inactiveIconName: "messageIconInactive")
+    static let tabBarMessageItemModel = PrimaryItemTabBarSecondaryItemModel(tabPosition: 3,
+                                                                            activeIconName: "messageIconActive",
+                                                                            inactiveIconName: "messageIconInactive",
+                                                                            activeColor: .blueFull,
+                                                                            inactiveColor: .black)
     
     static let tabBarProfileItemWrapper = PrimayItemTabBarItemWrapperModel(item: TabBarViewConstants.tabBarProfileItemModel)
-    static let tabBarProfileItemModel = PrimaryItemTabBarSecondaryItemModel(tabPosition: 4, activeIconName: "profileIconActive", inactiveIconName: "profileIconInactive")
+    static let tabBarProfileItemModel = PrimaryItemTabBarSecondaryItemModel(tabPosition: 4,
+                                                                            activeIconName: "profileIconActive",
+                                                                            inactiveIconName: "profileIconInactive",
+                                                                            activeColor: .blueFull,
+                                                                            inactiveColor: .black)
 }
--- GoAround/GoAround/Scenes/TabBar/View/GoAroundTabView.swift
+++ GoAround/GoAround/Scenes/TabBar/View/GoAroundTabView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CometUI
 
 struct GoAroundTabView: View {
     @Bindable var tabBarModel: PrimaryItemTabBarModel
